cf15ec1ae989ae068264c15de5f08eabf7e14788
shizidy <shizizuo1990>
2020-06-05 22:00:02 +0800

    first commit

---
+++ MediatorArchitectureTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MediatorArchitectureTest
+//
+//  Created by wdyzmx on 2020/6/5.
+//  Copyright © 2020 wdyzmx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
